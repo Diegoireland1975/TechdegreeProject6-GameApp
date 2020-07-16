6f5a0444a61e72e71176e1722ae1ae08332d94f3
Diego Raffa <diego170975@gmail.com>
2020-07-16 19:10:59 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,7 +1,33 @@
-let qwerty = $("#qwerty");
-let phrase = $("#prhase");
-let reset = $(".btn__reset");
+const qwerty = document.querySelector("#qwerty");
+const phrase = document.querySelector("#prhase");
+const startBtn = document.querySelector(".btn__reset");
+const overlay = document.querySelector("#overlay");
+let missed = 0;
+const phrases = [
+  "Css is Awesome",
+  "Javascript is not easy at the beginning",
+  "I wanna be a Front End Developer",
+  "You learn something new every day",
+  "I love Coding Pizza and IceCream",
+];
 
-let i = 0;
-i < guess.length;
-i++;
+const win = document.createElement("h3");
+win.textContent = "You win Congratulations";
+
+const lose = document.createElement("h3");
+lose.textContent = "You are a Looser!!!";
+
+startBtn.addEventListener("click", () => {
+  overlay.style.display = "none";
+});
+
+// Math.floor(Math.random() * 5 + 1);
+function getRandomPhraseAsArray(array) {
+  //random number based on length of array
+  let randomNum = Math.floor(Math.random() * Math.floor(array.length));
+  let randomPhrase = array[randomNum];
+  console.log(randomPhrase);
+  return randomPhrase;
+}
+
+getRandomPhraseAsArray(phrases);
